132ad57b9e204c1f3a0523b499bda0fe5e5dd7a2
rodrigoaderne <adernerodrigo@gmail.com>
2017-05-29 15:46:01 -0300

    Update exp3.ino

--- exp3.ino
+++ exp3.ino
@@ -1,11 +1,28 @@
+int pinoMotor = 3;
+int valorPWM = 0;
+//VARIAVEIS PARA BOTAO
 
-int pinoMotor = 3; //pino 3 ~PWM para controlar velocidade do motor
-int valorPWM = 0; 
+int verm_carro = 8;   //PIN DIGITAL 8
+int button = 2; // PIN DIGITAL 2
+int IR_COUNT = 0;
+
+void interrupcao(){ 
+   IR_COUNT++; //FLAG PARA INICIAR CONTADOR.
+   digitalWrite(verm_carro, HIGH);
+  } 
+
 void setup(){
-    pinMode(pinoMotor, OUTPUT); 
-}
- 
+  pinMode(verm_carro, OUTPUT);
+  
+  pinMode(pinoMotor, OUTPUT);
+  attachInterrupt(0, interrupcao, RISING); //Configurando a interrupção para botão, borda de descida.
+  }
+
 void loop(){
-    valorPWM = 255;
-    analogWrite(pinoMotor, valorPWM);
-}
+  valorPWM = 100;
+  analogWrite(pinoMotor, valorPWM);
+
+  if (IR_COUNT > 3){
+     
+    }
+  }
